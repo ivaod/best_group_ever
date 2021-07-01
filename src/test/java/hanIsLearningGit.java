0e42275c7d80b9236fdedf165463d18baa15fa02
aocal <A.OCAL88@GMAIL.COM>
2021-07-01 02:00:45 -0400

    han initial2

--- src/test/java/hanIsLearningGit.java
+++ src/test/java/hanIsLearningGit.java
@@ -2,7 +2,7 @@ public class hanIsLearningGit {
 
     public static void main(String[] args) {
 
-        System.out.println("Git gideceksen bekleme!!");
+        System.out.println("Git gideceksen bekleme");
 
     }
 }
